392d3dec44da89c439a0ad0cf3e38d32939529eb
Jazreel <jazreelsiew.2015@sis.smu.edu.sg>
2018-09-10 19:07:16 +0800

    Update all codes to depolyment

---
+++ realestate/js/compareProp.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function compareFunction() {
+    var propA = document.getElementById("PropertyAinput").value;
+    var propB = document.getElementById("PropertyBinput").value;
+//    console.log(propB);
+    squarefoot.load("sqftmain",{
+        task: "project", name: propA, type: "r", map: "1", nearby: "1,1,0,"+propB
+    });
+    $("#sqftmain").iFrameResize();
+}
